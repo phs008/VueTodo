60e08c6cc202c84aa55d2d96c68c692329c060a8
Joshua Park <phs008@naver.com>
2019-01-01 22:52:42 +0900

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <ul>
+            <li v-for="todoItem in todoItems" :key="todoItem.id">{{ todoItem }}</li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data()
+    {
+        return { todoItems: [] 
+        }
+    },
+    created()
+    {
+        if(localStorage.length > 0)
+        {
+            for(var i=0; i< localStorage.length; i++)
+            {
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
