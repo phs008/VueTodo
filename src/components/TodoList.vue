15d73e37c32c5bc0e96ce0fe07b9c53b83fc7833
Joshua Park <phs008@naver.com>
2019-01-02 23:24:50 +0900

    두번째

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
     <section>
         <ul>
-            <li v-for="todoItem in todoItems" :key="todoItem.id">{{ todoItem }}</li>
+            <li v-for="(todoItem,index) in todoItems" :key="todoItem" class="shadow">
+                <i class="checkBtn fa fa-check" aria-hidden="true"/>
+                {{todoItem}}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem,index)">
+                    <i class="fa fa-trash-o" aria-hidden="true"/>
+                </span>
+            </li>
         </ul>
     </section>
 </template>
@@ -22,10 +28,42 @@ export default {
                 this.todoItems.push(localStorage.key(i));
             }
         }
+    },
+    methods : {
+        removeTodo(todoItem,index)
+        {
+            console.log(todoItem,index);
+            localStorage.removeItem(todoItem);
+            this.todoItems.splice(index,1);
+        }
     }
 }
 </script>
 
 <style>
-
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+    .removeBtn{
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
